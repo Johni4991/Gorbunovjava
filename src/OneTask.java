98ac98d8b0d6e298cc3e1662301530c985f4f2d1
Evgeniy <83971292+Johni4991@users.noreply.github.com>
2024-02-14 20:31:14 +0400

    Update OneTask.java

--- src/OneTask.java
+++ src/OneTask.java
@@ -4,7 +4,7 @@ public class OneTask {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Введи число: ");
-        int x = s.nextInt();    //s.nextLine - для текста
+        int x = s.nextInt();  
         if (x > 7) {
             System.out.println("Привет");
         }
